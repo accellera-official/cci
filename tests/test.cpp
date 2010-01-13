39caaa78a1d84714c1cb6baf0fac0b8df8c6ce58
Christian Schröder <christian.schroeder@greensocs.com>
2010-01-13 08:35:23 +0000

    added test

---
+++ tests/test.cpp
@@ -0,0 +1,21 @@
+/*
+ *  json.cpp
+ *  greenstarcore
+ *
+ *  Created by Christian Schroeder on 13.01.10.
+ *  Copyright 2010 TU Braunschweig, E.I.S.. All rights reserved.
+ *
+ */
+
+//#include "cci.h"
+#include "gs_cci.h"
+
+
+using namespace std;
+
+
+int sc_main(int argc, char *argv[]) {
+
+  
+  return EXIT_SUCCESS; 
+}
