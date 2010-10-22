0f7120f35c713e8c3d2d436333e891283d1e35be
Christian Schröder <christian.schroeder@greensocs.com>
2010-10-22 08:44:43 +0000

    small Mikefile docs

--- gs_param_implementation/make_full_big_lib.sh
+++ gs_param_implementation/make_full_big_lib.sh
@@ -14,20 +14,27 @@ do
   echo; echo
   echo -n "make bigsymbols with bounds $lower_bound - $upper_bound "
   echo
-  echo make bigsymbols "CCI_SYMBOLS_LOWER_BOUND=$lower_bound" "CCI_SYMBOLS_UPPER_BOUND=$upper_bound" OBJNAME=big$lower_bound
-  make bigsymbols "CCI_SYMBOLS_LOWER_BOUND=$lower_bound" "CCI_SYMBOLS_UPPER_BOUND=$upper_bound" OBJNAME=big$lower_bound
+  echo   make bigsymbols "CCI_SYMBOLS_LOWER_BOUND=$lower_bound" "CCI_SYMBOLS_UPPER_BOUND=$upper_bound" OBJNAME=big$lower_bound_$upper_bound
+  make bigsymbols "CCI_SYMBOLS_LOWER_BOUND=$lower_bound" "CCI_SYMBOLS_UPPER_BOUND=$upper_bound" OBJNAME=big$lower_bound_$upper_bound
 done
 
 echo; echo
 echo -n "make the rest (make all)"
+echo
 make all
 
 echo; echo
-echo -n "Create table of contents"
+echo -n "Create library table of contents"
+echo -n "  ranlib libcciparamimpl.a"
+echo
 ranlib libcciparamimpl.a
 
 echo; echo
+echo -n "Strip library"
+echo -n "  strip -x -S libcciparamimpl.a -o libcciparamimpl.stripped.a"
+echo
 strip -x -S libcciparamimpl.a -o libcciparamimpl.stripped.a
 
 echo; echo
-echo -n "Compressing brings significant reduction in size!"
+echo -n "NOTE: Compressing brings significant reduction in size!"
+echo
