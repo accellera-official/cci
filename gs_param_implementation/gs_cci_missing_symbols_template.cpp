b074e8237f39a8569a5ecd076ac723d09fddae75
Christian Schröder <christian.schroeder@greensocs.com>
2010-11-01 13:17:55 +0000

    Added demonstration Makefile only compiling the needed (missing) symbols dynamically dependent on user code

---
+++ gs_param_implementation/gs_cci_missing_symbols_template.cpp
@@ -0,0 +1,24 @@
+// LICENSETEXT
+//
+//   Copyright (C) 2010 : GreenSocs Ltd
+// 	 http://www.greensocs.com/ , email: info@greensocs.com
+//
+//   Developed by:
+//    Christian Schroeder <schroeder@eis.cs.tu-bs.de>,
+//
+//
+// The contents of this file are subject to the licensing terms specified
+// in the file LICENSE. Please consult this file for restrictions and
+// limitations that may apply.
+// 
+// ENDLICENSETEXT
+
+
+//
+// File to be used for missing symbol customized symbol creation
+// e.g. see Makefile.only.needed.symbols in dev_examples/gs_example
+//
+
+#include "cci_params.h"
+
+namespace cci { namespace cnf {
