e7f05cf9cd93f6e9b48b35a3bdc11774729e9c50
Philipp A. Hartmann <philipp.hartmann@offis.de>
2014-11-19 21:36:10 +0100

    cci_value_traits: add basic support for sc_time
    Querying an sc_time object for its canonical value and
    constructing an sc_time object from a unit string would
    be helpful additions to the core language.

--- api/cci_cnf/cci_value_traits.cpp
+++ api/cci_cnf/cci_value_traits.cpp
@@ -64,6 +64,89 @@ DEFINE_BUILTIN_(std::string, string)
 // ----------------------------------------------------------------------------
 // SystemC builtin types
 
+DEFINE_PACK_( sc_core::sc_time )
+{
+  ///@todo normalize output to best matching unit
+  ///      (could benefit from sc_time extension)
+  dst.set_list()
+    .push_back( src.to_seconds() )
+    .push_back( "s" );
+  return true;
+}
+
+
+static inline sc_core::sc_time sc_time_from_unit( unsigned unit )
+{
+  ///@todo add error handling
+  sc_assert( unit <= sc_core::SC_SEC );
+  sc_core::sc_time_unit tu = static_cast<sc_core::sc_time_unit>( unit );
+  return sc_core::sc_time( 1, tu );
+}
+
+static inline sc_core::sc_time sc_time_from_symbol( cci_value_string_cref sym )
+{
+  static struct symbol
+  {
+    const char *          sym[2];
+    sc_core::sc_time_unit unit;
+  }
+  symbol_table[] =
+  {
+    { { "fs", "SC_FS"  }, sc_core::SC_FS  },
+    { { "ps", "SC_PS"  }, sc_core::SC_PS  },
+    { { "ns", "SC_NS"  }, sc_core::SC_NS  },
+    { { "us", "SC_US"  }, sc_core::SC_US  },
+    { { "ms", "SC_MS"  }, sc_core::SC_MS  },
+    { { "s",  "SC_SEC" }, sc_core::SC_SEC },
+    { { "sec" },          sc_core::SC_SEC },
+    { { NULL, NULL },     sc_core::sc_time_unit() }
+  };
+  symbol* cursor = symbol_table;
+  while( *cursor->sym && !(sym == cursor->sym[0] || sym == cursor->sym[1] ) )
+    cursor++;
+
+  sc_assert( unsigned(cursor - symbol_table) < (sizeof(symbol_table)/sizeof(symbol) ) );
+  return sc_time_from_unit( unsigned(cursor - symbol_table) );
+}
+
+
+DEFINE_UNPACK_( sc_core::sc_time )
+{
+  cci_value value, unit;
+  // encoded as tuple
+  if( src.is_list() && src.get_list().size() == 2 )
+  {
+    cci_value::const_list_reference l = src.get_list();
+      value = l[0];
+      unit  = l[1];
+  }
+  // encoded as map: "value"=, "unit"=
+  else if( src.is_map() && src.get_map().size() == 2 )
+  {
+    cci_value::const_map_reference m = src.get_map();
+    if( m.has_entry("value") && m.has_entry("unit") )
+    {
+      value = m["value"];
+      unit  = m["unit"];
+    }
+  }
+
+  if( !value.is_number() )
+    return false;
+
+  if( unit.is_string() )
+  {
+    dst = value.get_double() * sc_time_from_symbol( unit.get_string() );
+    return true;
+  }
+  else if( unit.is_uint() && unit.get_uint() <= sc_core::SC_SEC )
+  {
+    dst = value.get_double() * sc_time_from_unit( unit.get_uint() ) ;
+    return true;
+  }
+  return false;
+}
+
 DEFINE_BUILTIN_( sc_dt::sc_bit, bool )
 
 DEFINE_PACK_( sc_dt::sc_logic )
