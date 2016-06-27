cbcf7ef270db94bb2cf2b7ec3d09ee87e7a26309
eleilia <lei.liang@ericsson.com>
2016-06-27 19:05:35 +0200

    add copyright

--- examples/ex16_User_Defined_Data_Type/ex16_parameter_owner.h
+++ examples/ex16_User_Defined_Data_Type/ex16_parameter_owner.h
@@ -5,6 +5,9 @@
   Copyright 2010-2015 CircuitSutra Technologies Pvt. Ltd.
   All rights reserved.
 
+  Copyright 2016 Ericsson AB.
+  All rights reserved.
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
--- src/cci_cfg/cci_base_param.h
+++ src/cci_cfg/cci_base_param.h
@@ -11,6 +11,9 @@ All rights reserved.
 Copyright 2006-2015 Intel Corporation
 All rights reserved.
 
+Copyright 2016 Ericsson AB
+All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
