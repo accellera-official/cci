82928df32a52843ca24ed0a789c394c1381bbfb9
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2016-06-29 22:43:06 +0200

    dev_examples: update license to Apache 2 (donation)
    GreenSocs donation to Accellera. Update license to
    Apache 2.
    
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>
    Signed-off-by: Mark Burton <mark.burton@greensocs.com>

--- dev_examples/cci_value_example/gs_cci_symbols_c.cpp
+++ dev_examples/cci_value_example/gs_cci_symbols_c.cpp
@@ -1,17 +1,27 @@
-// LICENSETEXT
-//
-//   Copyright (C) 2011 : GreenSocs Ltd
-// 	 http://www.greensocs.com/ , email: info@greensocs.com
-//
-//   Developed by:
-//    Christian Schroeder <schroeder@eis.cs.tu-bs.de>,
-//
-//
-// The contents of this file are subject to the licensing terms specified
-// in the file LICENSE. Please consult this file for restrictions and
-// limitations that may apply.
-// 
-// ENDLICENSETEXT
+/*****************************************************************************
+  Copyright 2016 Accellera Systems Initiative Inc.
+  All rights reserved.
+
+  Copyright 2011 GreenSocs
+  All rights reserved.
+  Authors:
+      Christian Schroeder <schroeder@eis.cs.tu-bs.de>
+
+  Copyright 2016 Ericsson
+  All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ *****************************************************************************/
 
 #define SC_INCLUDE_DYNAMIC_PROCESSES
 #include "gs_cci_symbols_c.h"
