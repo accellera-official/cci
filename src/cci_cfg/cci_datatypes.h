23a5ddf6c2048803e0c005ebb05b7fda81a88096
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2016-06-30 10:04:44 +0200

    src: update license to Apache 2 (donation)
    GreenSocs donation to Accellera.
    Update license to Apache 2.
    
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>
    Signed-off-by: Mark Burton <mark.burton@greensocs.com>

--- src/cci_cfg/cci_datatypes.h
+++ src/cci_cfg/cci_datatypes.h
@@ -1,18 +1,28 @@
-// LICENSETEXT
-//
-//   Copyright (C) 2010 : GreenSocs Ltd
-// 	 http://www.greensocs.com/ , email: info@greensocs.com
-//
-//   Developed by:
-//    Christian Schroeder <schroeder@eis.cs.tu-bs.de>,
-//    Mark Burton, mark@greensocs.com
-//
-//
-// The contents of this file are subject to the licensing terms specified
-// in the file LICENSE. Please consult this file for restrictions and
-// limitations that may apply.
-// 
-// ENDLICENSETEXT
+/*****************************************************************************
+  Copyright 2016 Accellera Systems Initiative Inc.
+  All rights reserved.
+
+  Copyright 2010 GreenSocs
+  All rights reserved.
+  Authors:
+      Christian Schroeder <schroeder@eis.cs.tu-bs.de>
+      Mark Burton <mark@greensocs.com>
+
+  Copyright 2016 Ericsson
+  All rights reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ *****************************************************************************/
 
 #ifndef CCI_CCI_DATATYPES_H_INCLUDED_
 #define CCI_CCI_DATATYPES_H_INCLUDED_
