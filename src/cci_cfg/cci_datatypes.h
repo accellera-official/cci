1a65bac9e28baf36bdd578be9155964f3027feba
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2016-10-14 17:47:00 +0200

    cci_datatypes: fix indentation and extra whitespaces
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>

--- src/cci_cfg/cci_datatypes.h
+++ src/cci_cfg/cci_datatypes.h
@@ -24,27 +24,26 @@
 
 CCI_OPEN_NAMESPACE_
 
-  /// Enumeration for basic data types, whose rough getting and setting is supported by base class directly
-  enum basic_param_type {
+/// Enumeration for basic data types, whose rough getting and setting is supported by base class directly
+enum basic_param_type {
     /// default, type unknown
-    param_type_not_available = 0, 
+    param_type_not_available = 0,
     /// used for all param data types that can handle integer numbers
-    param_type_number,   
+    param_type_number,
     /// used for all param data types that can handle real numbers
-    param_type_real,     
+    param_type_real,
     /// used for all param data types that can handle bool
-    param_type_bool,     
+    param_type_bool,
     /// used for all param data types that can handle strings
-    param_type_string,   
-    /// used for all param lists 
-    param_type_list,     
+    param_type_string,
+    /// used for all param lists
+    param_type_list,
     /// used for all param other data types (objects etc.)
-    param_type_other     
-  };
-  
-  
-  /// Enumeration for cci_param_typed template specifying the parameter type according the lock behavior
-  enum cci_param_mutable_type {
+    param_type_other
+};
+
+/// Enumeration for cci_param_typed template specifying the parameter type according the lock behavior
+enum cci_param_mutable_type {
     /// Mutable Parameter
     CCI_MUTABLE_PARAM = 0,
     /// Immutable Parameter
@@ -53,18 +52,18 @@ CCI_OPEN_NAMESPACE_
     CCI_ELABORATION_TIME_PARAM,
     /// Vendor specific/other Parameter type
     CCI_OTHER_PARAM
-  };
+};
 
-  /**
-   * Enumeration for convenient cci_param_typed constructor.
-   * Choose if a param gets a relative or an absolute name.
-   */
-  enum cci_name_type {
+/**
+ * Enumeration for convenient cci_param_typed constructor.
+ * Choose if a param gets a relative or an absolute name.
+ */
+enum cci_name_type {
     /// Relative name
     CCI_RELATIVE_NAME,
     /// Absolute name
     CCI_ABSOLUTE_NAME
-  };
+};
 
 
 CCI_CLOSE_NAMESPACE_
