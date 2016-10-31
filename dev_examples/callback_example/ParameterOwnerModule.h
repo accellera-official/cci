1dee82d883a9ef301b33690767a76c2054359c33
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2016-10-31 22:44:21 +0100

    dev_examples/callback_example: catch bad cci_value during parameter construction
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>

--- dev_examples/callback_example/ParameterOwnerModule.h
+++ dev_examples/callback_example/ParameterOwnerModule.h
@@ -48,10 +48,25 @@ public:
   , uint_param2("uint_param2", 12)
   , str_param("str_param", cci::cci_value("This is a test string."))
   , bool_param("bool_param", false)
-  , top_param("top.arbitrary.param", cci::cci_value("This is a param with top-level-name") , 
-	      "A string parameter with absolute name.", cci::CCI_ABSOLUTE_NAME)
-  { 
-    SC_THREAD(main_action);
+  {
+      SC_THREAD(main_action);
+
+      try {
+          top_param = new cci::cci_param<int>("top.arbitrary.param",
+                                              cci::cci_value(
+                                                      "This is a param with top-level-name"),
+                                              "A string parameter with absolute name.",
+                                              cci::CCI_ABSOLUTE_NAME);
+      }
+      catch (sc_core::sc_report e) {
+          std::cout << std::endl << name
+                    << ": Unable to initialize 'Owner.top_param'" << std::endl;
+          switch (cci::cci_report_handler::get_param_failure(e)) {
+            case cci::CCI_VALUE_FAILURE:
+              std::cout << e.what() << std::endl;
+              break;
+          }
+      }
   }
   
   /// Main action to make tests with parameters.
@@ -68,7 +83,7 @@ public:
   /// Example parameter.
   cci::cci_param<bool>            bool_param;
   /// Example parameter for top-level-name
-  cci::cci_param<int>             top_param;
+  cci::cci_param<int>*            top_param;
 
 };
 
