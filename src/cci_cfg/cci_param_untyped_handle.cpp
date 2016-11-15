dfdbd14be318ae173a452ca6b87b3bb557459576
Guillaume Delbergue <guillaume.delbergue@greensocs.com>
2016-11-15 10:42:23 +0100

    cci_param_untyped_handle: protect operator= against self assignement
    Signed-off-by: Guillaume Delbergue <guillaume.delbergue@greensocs.com>

--- src/cci_cfg/cci_param_untyped_handle.cpp
+++ src/cci_cfg/cci_param_untyped_handle.cpp
@@ -68,7 +68,7 @@ cci_param_untyped_handle& cci_param_untyped_handle::operator=(
     m_orig_param = param_handle.m_orig_param;
     m_orig_param_name = param_handle.m_orig_param_name;
 
-    if(is_valid()) {
+    if((this != &param_handle) && is_valid()) {
         m_orig_param->add_param_handle(this);
     }
     return *this;
