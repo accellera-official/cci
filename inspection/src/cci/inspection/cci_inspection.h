b4ba045aaa07eda66eed51725f58ed9af6a3aea2
Antonios Salios <antonios.salios@rwth-aachen.de>
2025-08-06 08:24:29 +0200

    add missing license headers
    Signed-off-by: Antonios Salios <antonios.salios@rwth-aachen.de>

--- inspection/src/cci/inspection/cci_inspection.h
+++ inspection/src/cci/inspection/cci_inspection.h
@@ -1,12 +1,21 @@
-/******************************************************************************
- *                                                                            *
- * Copyright (C) 2025 CCI Working Group
- * All Rights Reserved                                                        *
- *                                                                            *
- * This is work is licensed under the terms described in the LICENSE file     *
- * found in the root directory of this source tree.                           *
- *                                                                            *
- ******************************************************************************/
+/*****************************************************************************
+
+  Licensed to Accellera Systems Initiative Inc. (Accellera) under one or
+  more contributor license agreements.  See the NOTICE file distributed
+  with this work for additional information regarding copyright ownership.
+  Accellera licenses this file to you under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with the
+  License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+  implied.  See the License for the specific language governing
+  permissions and limitations under the License.
+
+ ****************************************************************************/
 
 #ifndef CCI_INSPECTION_H
 #define CCI_INSPECTION_H
